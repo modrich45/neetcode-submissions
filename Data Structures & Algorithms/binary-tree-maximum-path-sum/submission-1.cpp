4cf8795d791e13ec919adf0a88ffac08293005c7
Vishal Kumar <101268541+modrich45@users.noreply.github.com>
2026-07-22 23:12:22 +0530

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int solve(TreeNode* root, int &ans){
+        if(root==NULL){
+            return 0;
+        }
+
+        int left=max(solve(root->left,ans),0);
+        int right=max(solve(root->right,ans),0);
+        ans=max(ans,left+right+root->val);
+        return root->val+max(left,right);
+    }
+    int maxPathSum(TreeNode* root) {
+        int ans=INT_MIN;
+        solve(root,ans);
+        return ans;
+    }
+};
