ea533500b17f19b6c486a321c0e89cdaad27731e
Vishal Kumar <101268541+modrich45@users.noreply.github.com>
2026-07-29 00:31:16 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    set<vector<int>>ans;
+    void solve(vector<int>& nums,int i, int target,int n,int curr,vector<int>&temp){
+        if(target==curr){
+            vector<int> t = temp;
+            sort(t.begin(), t.end());
+            ans.insert(t);
+            return;
+        }
+        if(i<0) return;
+
+        if(curr<target){
+            solve(nums,i-1,target,n,curr,temp);
+            temp.push_back(nums[i]);
+            solve(nums,i,target,n,curr+nums[i],temp);
+            solve(nums,i-1,target,n,curr+nums[i],temp);
+            temp.pop_back();
+        }else{
+            return;
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>temp;
+        solve(nums,nums.size()-1,target,nums.size(),0,temp);
+        vector<vector<int>>res;
+        for(auto it=ans.begin();it!=ans.end();it++){
+            res.push_back((*it));
+        }
+        return res;
+    }
+};
