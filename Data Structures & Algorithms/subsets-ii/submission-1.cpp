d75669055a28e096c3b779c024362407b725904b
Vishal Kumar <101268541+modrich45@users.noreply.github.com>
2026-07-30 23:33:27 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    set<vector<int>> res;
+    void solve(vector<int>& nums, int i, int n, vector<int>& temp) {
+        res.insert(temp);
+
+        for (int j = i; j < n; j++) {
+            if (j > i && nums[j] == nums[j - 1])
+                continue;
+            temp.push_back(nums[j]);
+            solve(nums, j + 1, n, temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> temp;
+        int n = nums.size();
+        solve(nums, 0, n, temp);
+
+        return vector<vector<int>>(res.begin(), res.end());
+    }
+};
